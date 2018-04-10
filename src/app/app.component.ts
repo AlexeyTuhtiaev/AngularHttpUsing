7e5cdee29a192d92a9b1fca505d5147a6f279d32
alexey.tuhtiaev <alexey.tuhtiaev@gmail.com>
2018-04-10 20:30:19 +0300

    initial project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
  cars = [
    {
-     name: '',
-     color: '',
-     id: ''
+     name: 'Ford',
+     color: 'white',
+     id: '1'
    }
  ];
 }
